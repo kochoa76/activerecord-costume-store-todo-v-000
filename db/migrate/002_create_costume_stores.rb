bacdfe625c028a0753cfac35878aa7c1a970ec85
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-03 02:30:41 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CreateCostumeStore < ActiveRecord::Migration 
+
+  def change 
+    create_table :costumeStores do |t| 
+      t.string :name 
+      t.string :location 
+      t.integer :costume_inventory 
+      t.integer :num_of_employees 
+      t.boolean :still_in_business 
+      t.timestamps 
+  end 
+end 
