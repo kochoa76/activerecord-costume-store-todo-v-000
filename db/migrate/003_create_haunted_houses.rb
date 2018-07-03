d1cd25a41f9f1281b4262ac07766976f82f63e45
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-03 02:41:40 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,5 +10,6 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
+      t.string :description 
   end
 end
